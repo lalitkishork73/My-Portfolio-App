70322c437659935f812d3ecb8c9ea81820d97a26
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-10-27 17:16:32 +0530

    added all skilsDB

--- src/components/Skils.js
+++ src/components/Skils.js
@@ -8,7 +8,7 @@ const Skils = () => {
       <section>
         <div
           name="experience"
-          className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+          className="bg-gradient-to-b from-gray-800 to-black w-full h-auto"
         >
           <div className="max-w-screen-lg mx-auto p-4 flex flex-col w-full h-full text-white">
             <div>
