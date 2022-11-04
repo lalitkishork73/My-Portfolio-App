a18136559dd3be0a7592859631c4820b159327a1
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-11-04 20:25:02 +0530

    done

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,27 +1,30 @@
 const ProjectData = [
   {
     id: 1,
-    videoLink: "",
-    projectName: "",
-    description: "",
-    viewCode: "",
-    visitSite: "",
-    type:"BackEnd"
+    videoLink: `https://www.youtube.com/embed/LIIDh-qI9oI?list=RDLIIDh-qI9oI`,
+    projectName: "Block Management",
+    description: "You can also use variant modifiers to target media queries like responsive breakpoints, dark mode, prefers-reduced-motion, and more. For example, use md:place-content-center to apply the place-content-center utility at only medium screen sizes and above.",
+    viewCode: "https://www.google.co.in/",
+    visitSite: "https://github.com/lalitkishork73"
   },
   {
     id: 2,
-    videoLink: "",
-    projectName: "",
-    description: "",
-    viewCode: "",
-    visitSite: "",
+    videoLink: `https://www.youtube.com/embed/qFLhGq0060w?list=RDLIIDh-qI9oI`,
+    projectName: "Url Shortner",
+    description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:place-content-center to only apply the place-content-center utility on hover.",
+    viewCode: "https://www.google.co.in/",
+    visitSite: "https://github.com/lalitkishork73",
   },
   {
     id: 3,
-    videoLink: "",
-    projectName: "",
-    description: "",
-    viewCode: "",
-    visitSite: "",
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "Product Management",
+    description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
+    viewCode: "https://www.google.co.in/",
+    visitSite: "https://github.com/lalitkishork73",
   },
 ];
+
+export default ProjectData;
+
+<iframe width="1280" height="720" src="https://www.youtube.com/embed/qFLhGq0060w?list=RDLIIDh-qI9oI" title="The Weeknd - I Feel It Coming ft. Daft Punk (Official Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
