e4d4a064d787869476105df652e9e541a392f49f
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-03 19:58:29 +0530

    added

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -23,6 +23,30 @@ const ProjectData = [
     viewCode: "https://www.google.co.in/",
     visitSite: "https://github.com/lalitkishork73",
   },
+  {
+    id: 4,
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "Book Management",
+    description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
+    viewCode: "https://www.google.co.in/",
+    visitSite: "https://github.com/lalitkishork73",
+  },
+  {
+    id: 5,
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "E-commerce Shoping cart",
+    description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
+    viewCode: "https://www.google.co.in/",
+    visitSite: "https://github.com/lalitkishork73",
+  },
+  {
+    id: 6,
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "Web Application",
+    description: "This app is collection of multiple web applications based on pure html, css and javascript.",
+    viewCode: "https://github.com/lalitkishork73/Web_Assignments/tree/main",
+    visitSite: "https://lalitkisho.000webhostapp.com/",
+  },
 ];
 
 export default ProjectData;
