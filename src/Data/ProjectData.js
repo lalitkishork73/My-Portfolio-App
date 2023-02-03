1d981b86f3591e73e95ebe5202e705e161938364
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-03 21:11:25 +0530

    updated some project

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -4,7 +4,7 @@ const ProjectData = [
     videoLink: `https://www.youtube.com/embed/LIIDh-qI9oI?list=RDLIIDh-qI9oI`,
     projectName: "Blog Management",
     description: "Blog-management",
-    viewCode: "https://www.google.co.in/",
+    viewCode: "https://github.com/lalitkishork73/Project-1-Blog-Site",
     visitSite: "https://github.com/lalitkishork73"
   },
   {
@@ -12,15 +12,15 @@ const ProjectData = [
     videoLink: `https://www.youtube.com/embed/qFLhGq0060w?list=RDLIIDh-qI9oI`,
     projectName: "Url Shortner",
     description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:place-content-center to only apply the place-content-center utility on hover.",
-    viewCode: "https://www.google.co.in/",
+    viewCode: "https://github.com/lalitkishork73/Project-4-URL_Shortner",
     visitSite: "https://github.com/lalitkishork73",
   },
   {
     id: 3,
     videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
-    projectName: "Product Management",
+    projectName: "OpenToIntern",
     description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
-    viewCode: "https://www.google.co.in/",
+    viewCode: "https://github.com/lalitkishork73/Project-2-OpenToIntern",
     visitSite: "https://github.com/lalitkishork73",
   },
   {
@@ -28,7 +28,7 @@ const ProjectData = [
     videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
     projectName: "Book Management",
     description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
-    viewCode: "https://www.google.co.in/",
+    viewCode: "https://github.com/lalitkishork73/Project-3-Book-Managment",
     visitSite: "https://github.com/lalitkishork73",
   },
   {
@@ -36,7 +36,7 @@ const ProjectData = [
     videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
     projectName: "E-commerce Shoping cart",
     description: "Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:grid-flow-row to only apply the grid-flow-row utility on hover.",
-    viewCode: "https://www.google.co.in/",
+    viewCode: "https://github.com/lalitkishork73/Project-5-Product-Management",
     visitSite: "https://github.com/lalitkishork73",
   },
   {
@@ -45,6 +45,22 @@ const ProjectData = [
     projectName: "Web Application",
     description: "This app is collection of multiple web applications based on pure html, css and javascript.",
     viewCode: "https://github.com/lalitkishork73/Web_Assignments/tree/main",
+    visitSite: "https://lalitkisho.000webhostapp.com/",
+  },
+  {
+    id: 7,
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "Youtube Clone",
+    description: "This app is collection of multiple web applications based on pure html, css and javascript.",
+    viewCode: "https://github.com/lalitkishork73/ReactApps",
+    visitSite: "https://lalitkisho.000webhostapp.com/",
+  },
+  {
+    id: 8,
+    videoLink: `https://www.youtube.com/embed/H5v3kku4y6Q?list=RDLIIDh-qI9oI`,
+    projectName: "Flask Application",
+    description: "This app is collection of multiple web applications based on pure html, css and javascript.",
+    viewCode: "https://github.com/lalitkishork73/Flask-",
     visitSite: "https://lalitkisho.000webhostapp.com/",
   },
 ];
